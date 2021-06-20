6df5d34a88bfb47576287c21d0525152d7ce62df
Apple <apple@MacBook-Pro.local>
2021-06-20 14:01:40 +0700

    Create Vinhomes project

---
+++ VinHomes/MainScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainScreenViewController.swift
+//  VinHomes
+//
+//  Created by Apple on 20/06/2021.
+//
+
+import UIKit
+
+class MainScreenViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VinHomes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VinHomes
+//
+//  Created by Apple on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
